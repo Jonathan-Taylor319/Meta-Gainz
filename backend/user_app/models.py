b0e962734613bb19c9f7c69c1f23bf387adb64f4
Jonathan-Taylor319 <jontaylor319@gmail.com>
2025-06-12 13:39:14 -0400

    went with builtin user, adding profile

--- backend/user_app/models.py
+++ backend/user_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+# We are going to use Django's built in User Model, so now we need to import it
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pics/')
+    total_weight_lifted = models.IntegerField()
+    personal_tagline = models.TextField(max_length=400)
