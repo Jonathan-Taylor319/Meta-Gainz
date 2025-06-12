dbe3794301f726094bed02ba8cb9c48a84d82019
Jonathan-Taylor319 <jontaylor319@gmail.com>
2025-06-12 09:54:34 -0400

    added apps and started backend

---
+++ backend/meta_gainz_proj/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for meta_gainz_proj project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meta_gainz_proj.settings')
+
+application = get_wsgi_application()
